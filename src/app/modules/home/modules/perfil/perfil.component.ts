28f9162fc9f7e60b16124722a46c0783bb5c9f22
Maveto13 <maveto13@gamil.com>
2020-12-20 22:40:06 -0400

    Delete y Update de usuarios

--- src/app/modules/home/modules/perfil/perfil.component.ts
+++ src/app/modules/home/modules/perfil/perfil.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.css']
 })
-export class PerfilComponent implements OnInit {
+export class PerfilComponent implements OnInit, OnDestroy{
 
-  constructor() { }
+  formulario: FormGroup;
+  getUserSub: Subscription;
+  updateSub: Subscription;
+  idTabla;
+
+  constructor(private formBuilder: FormBuilder, private userSer: UserService, private auth: AuthService) { }
 
   ngOnInit() {
+    this.formulario = this.formBuilder.group({
+      nombre: "",
+      email: "",
+      direccion: "",
+      numero: ""
+    });
+    this.loadData();
+  }
+
+  loadData() {
+    this.getUserSub = this.userSer.getUsersById(this.auth.getUserId()).subscribe(
+      res => {
+        console.log(this.formulario.value)
+        Object.entries(res).map((p:any) => {
+          console.log(p)
+          this.idTabla = p[0];
+          this.formulario.patchValue({
+            nombre: p[1].name,
+            email: p[1].mail,
+            direccion: p[1].direction,
+            numero: p[1].number
+          });
+          
+        });
+      }
+    );
+  }
+
+  updateProfile() {
+    this.updateSub = this.userSer.updateUser(this.idTabla, {
+      name: this.formulario.value.nombre,
+      mail: this.formulario.value.email,
+      direction: this.formulario.value.direccion,
+      number: this.formulario.value.numero,
+      id: this.auth.getUserId(),
+      type: "Empresa"
+    }).subscribe(
+      res => {
+        console.log("Update Success");
+        this.loadData();
+      }
+    )
+  }
+
+  ngOnDestroy(){
+    this.getUserSub ? this.getUserSub.unsubscribe() : "";
   }
 
 }
